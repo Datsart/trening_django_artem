ec0d8edcb4b05942977cfb6acfd9aa39c2a2c74a
darkkrogonk <darkkrogonk@gmail.com>
2023-05-08 11:03:51 +0300

    Добавили рубрики - создали модель, привязали к объявлениям, зарегали в админке в модели объявления создали поле для связи с моделью рубрики

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Bb
+from .models import Bb, Rubric
+
+
 # Register your models here.
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price','published']
+    list_display = ['title', 'rubric', 'price', 'published']
 
 
 admin.site.register(Bb, BbAdmin)
+admin.site.register(Rubric)
+
--- bboard/models.py
+++ bboard/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название Рубрики')
+
+    class Meta:
+        verbose_name = 'Название Рубрики'
+        verbose_name_plural = 'Названия Рубрик'
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
 class Bb(models.Model):
     title = models.CharField(max_length=50, verbose_name='Название объявления')
     content = models.TextField(null=True, blank=True, verbose_name='Текст объявления')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публикации')
+    rubric = models.ForeignKey(Rubric, null=True, blank=True, verbose_name='Название Рубрики', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Объявление'
