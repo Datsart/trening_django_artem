9c47a058da36067c14ea2d63b2db9c9c20f1c85e
darkkrogonk <darkkrogonk@gmail.com>
2023-05-13 14:31:51 +0300

    pars

--- product/management/commands/pars_product.py
+++ product/management/commands/pars_product.py
@@ -1,19 +1,34 @@
 from django.core.management.base import BaseCommand
 import openpyxl
+from product.models import Product, Units
+from tqdm import tqdm
 
 
 class Command(BaseCommand):
     help = 'Импорт товаров из таблицы'
 
     def handle(self, *args, **options):
-        file_path = 'static/test1.xlsx'  # указываем имя файла
-        wb = openpyxl.load_workbook(file_path)
+        wb = openpyxl.load_workbook('test1.xlsx')
         ws = wb.active
+        total_rows = ws.max_row  # Вычитаем заголовок таблицы
+        total_products = 0
 
-        print("Книга:", wb)
-        print("Активный лист:", ws)
+        with tqdm(total=total_rows, unit='row') as pbar:
+            for row in ws.iter_rows(min_row=0):  # Начинаем с 2 строки, пропуская заголовок таблицы
+                name = row[0].value
+                amount = row[1].value
+                unit = row[2].value
+                if not name and not amount and not unit:
+                    break
+                else:
+                    units, _ = Units.objects.get_or_create(name=unit)
 
-        for row in ws.iter_rows(min_row=0):
-            name = row[0].value
-            if name:
-                print("Название товара:", name)
+                    product = Product.objects.create(
+                        title=name,
+                        amount=amount,
+                        unit=units
+                    )
+                    total_products += 1
+                    pbar.update(1)  # Обновляем прогресс-бар
+
+        self.stdout.write(self.style.SUCCESS(f'Успешно импортировано {total_products} товаров.'))
